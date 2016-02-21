ae9e39aeb919215c85a2aba138bd00f3064009a2
Evgeniy Popik <grindcoretm@gmail.com>
2016-02-21 21:36:49 +0200

    Update JSON_Solo_Themes_Promotion.js

--- JSON_Solo_Themes_Promotion.js
+++ JSON_Solo_Themes_Promotion.js
@@ -1,17 +1,17 @@
 {
     "wallpapers": [
 		  {
-            "name": "Garfield",
+            "name": "Fire",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_1.jpg",
 			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.garfieldsport"
           },
           {
-            "name": "Jackpot",
+            "name": "Black Silver",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_2.jpg",
 			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.jackpot2010"
           },
           {
-            "name": "Neon Lines",
+            "name": "3D Fresh Style",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_3.jpg",
 			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.neonlines2109"
           },
@@ -21,28 +21,63 @@
 			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.shine0910"
           },
           {
-            "name": "Smiley",
+            "name": "Neon Lines",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_5.jpg",
 			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.smilingface2709"
           },
           {
-            "name": "Thunder",
+            "name": "Hologram",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_6.jpg",
 			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.thunder0910"
           },
           {
-            "name": "Aroma Flower",
+            "name": "Thunder",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_7.jpg",
 			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.aromaflower"
           },
           {
-            "name": "Harmony",
+            "name": "MIU",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_8.jpg",
 			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.harmony"
           },
           {
-            "name": "Nuclear",
+            "name": "Whity",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_9.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+          },
+          {
+            "name": "Yose",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_10.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+          },
+          {
+            "name": "Jackpot",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_11.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+          },
+          {
+            "name": "Sunshine",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_12.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+          },
+          {
+            "name": "Leather",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_13.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+          },
+          {
+            "name": "Business Style",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_14.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+          },
+          {
+            "name": "Nuclear",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_15.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+          },
+          {
+            "name": "Quadro Blow",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_16.jpg",
 			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
           }
 		  
