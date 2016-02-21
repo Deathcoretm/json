2ebad108aeb290dd413f52a0d72c77d072579543
Evgeniy Popik <grindcoretm@gmail.com>
2016-02-21 21:52:19 +0200

    Update JSON_Solo_Themes_Promotion.js

--- JSON_Solo_Themes_Promotion.js
+++ JSON_Solo_Themes_Promotion.js
@@ -3,83 +3,82 @@
 		  {
             "name": "Fire",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_1.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.garfieldsport"
+	    "link": "http://tiny.cc/solofire"
           },
           {
             "name": "Black Silver",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_2.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.jackpot2010"
+	    "link": "http://tiny.cc/soloblacksilv"
           },
           {
             "name": "3D Fresh Style",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_3.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.neonlines2109"
+	    "link": "http://tiny.cc/solofreshstyle"
           },
           {
             "name": "Shine",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_4.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.shine0910"
+	    "link": "http://tiny.cc/soloshine"
           },
           {
             "name": "Neon Lines",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_5.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.smilingface2709"
+	    "link": "http://tiny.cc/soloneonlines"
           },
           {
             "name": "Hologram",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_6.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.thunder0910"
+	    "link": "http://tiny.cc/solohologram"
           },
           {
             "name": "Thunder",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_7.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.aromaflower"
+	    "link": "http://tiny.cc/solothunder"
           },
           {
             "name": "MIU",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_8.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.harmony"
+	    "link": "http://tiny.cc/solomiu"
           },
           {
             "name": "Whity",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_9.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+	    "link": "http://tiny.cc/solowhity"
           },
           {
             "name": "Yose",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_10.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+	    "link": "http://tiny.cc/soloyose"
           },
           {
             "name": "Jackpot",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_11.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+	    "link": "http://tiny.cc/solojackpot"
           },
           {
             "name": "Sunshine",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_12.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+	    "link": "http://tiny.cc/solosunshine"
           },
           {
             "name": "Leather",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_13.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+	    "link": "http://tiny.cc/sololeather"
           },
           {
             "name": "Business Style",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_14.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+	    "link": "http://tiny.cc/solobusiness"
           },
           {
             "name": "Nuclear",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_15.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+	    "link": "http://tiny.cc/solonuclear"
           },
           {
             "name": "Quadro Blow",
             "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_16.jpg",
-			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+	    "link": "http://tiny.cc/soloquadro"
           }
-		  
 	]
 }
