08f4ee02856af42253a38e67f2c23450716af505
Evgeniy Popik <grindcoretm@gmail.com>
2016-02-21 21:32:25 +0200

    Added files via upload

---
+++ JSON_Solo_Themes_Promotion.js
@@ -0,0 +1,50 @@
+{
+    "wallpapers": [
+		  {
+            "name": "Garfield",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_1.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.garfieldsport"
+          },
+          {
+            "name": "Jackpot",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_2.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.jackpot2010"
+          },
+          {
+            "name": "Neon Lines",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_3.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.neonlines2109"
+          },
+          {
+            "name": "Shine",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_4.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.shine0910"
+          },
+          {
+            "name": "Smiley",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_5.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.smilingface2709"
+          },
+          {
+            "name": "Thunder",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_6.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.solo.theme.thunder0910"
+          },
+          {
+            "name": "Aroma Flower",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_7.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.aromaflower"
+          },
+          {
+            "name": "Harmony",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_8.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.harmony"
+          },
+          {
+            "name": "Nuclear",
+            "image": "http://googledrive.com/host/0B7EE4ooGQrvyRV9Gb09KWEQ0cWc/prom_9.jpg",
+			"link": "https://play.google.com/store/apps/details?id=com.bz.solo.theme.nuclear"
+          }
+		  
+	]
+}
